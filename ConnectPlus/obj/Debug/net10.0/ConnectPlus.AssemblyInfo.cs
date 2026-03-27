bf2d18a800878403087e2656bea0df1a0db3a6b2
Felipe Gomes <felipegomes8819@gmail.com>
2026-03-27 09:34:02 -0300

    Adicionei o trello

--- ConnectPlus/obj/Debug/net10.0/ConnectPlus.AssemblyInfo.cs
+++ ConnectPlus/obj/Debug/net10.0/ConnectPlus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectPlus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0a92d47713144268bff35db4bc0d421bd706d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectPlus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectPlus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
